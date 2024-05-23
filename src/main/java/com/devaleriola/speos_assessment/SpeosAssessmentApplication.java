d27db10e8b6abee450bea7a9b648622f9957efc0
Devaleriola <devaleriolat@delawareconsulting.com>
2024-05-23 14:13:56 +0200

    Add initial project from Spring Initializr

---
+++ src/main/java/com/devaleriola/speos_assessment/SpeosAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.devaleriola.speos_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeosAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeosAssessmentApplication.class, args);
+	}
+
+}
