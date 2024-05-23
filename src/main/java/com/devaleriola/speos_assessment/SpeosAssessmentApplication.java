e8f930c1266a3ede368b313ed53a3b8cd1a00269
Devaleriola <devaleriolat@delawareconsulting.com>
2024-05-23 14:28:35 +0200

    Merge servlet initializer with spring boot application, add POM dependencies for JPA and OpenAPI

--- src/main/java/com/devaleriola/speos_assessment/SpeosAssessmentApplication.java
+++ src/main/java/com/devaleriola/speos_assessment/SpeosAssessmentApplication.java
@@ -2,12 +2,19 @@ package com.devaleriola.speos_assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpeosAssessmentApplication {
+public class SpeosAssessmentApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpeosAssessmentApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpeosAssessmentApplication.class);
 	}
 
 }
