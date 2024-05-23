ab781e56c8b9c0a3ea7423a30fdb051c5dc050b6
Devaleriola <devaleriolat@delawareconsulting.com>
2024-05-23 16:22:27 +0200

    Add health endpoint on /actuator/health

--- src/main/java/com/devaleriola/speos_assessment/services/test/TestService.java
+++ src/main/java/com/devaleriola/speos_assessment/services/test/TestService.java
@@ -2,8 +2,6 @@ package com.devaleriola.speos_assessment.services.test;
 
 import com.devaleriola.speos_assessment.entities.test.TestDto;
 import com.devaleriola.speos_assessment.services.GenericService;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface TestService extends GenericService<TestDto> {
 }
